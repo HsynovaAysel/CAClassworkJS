14273d6497bbad87780fb58a3f4213345c42dc01
aghalarova <agalarovasbin76@gmail.com>
2023-10-30 08:53:45 -0700

    js intro

---
+++ 01.Data-Types&Variables&Operators/main.js
@@ -0,0 +1,62 @@
+let firstName = "Aysel";
+let lastName = "Huseynova.";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let job = "student";
+let country = "Gence";
+let age = "21";
+let personalInfo =
+  fullName +
+  " " +
+  "I am a " +
+  job +
+  " " +
+  ". I live in " +
+  country +
+  " " +
+  ".I am " +
+  age +
+  " " +
+  "years old.";
+console.log(personalInfo);
+
+let lenght = 16;
+console.log(typeof lenght);
+let color = "Brown";
+console.log(typeof color);
+let float = "12.5";
+console.log(typeof float);
+let like = true;
+console.log(typeof like);
+let x = {
+  firstName: "Aysel",
+  color: "red",
+};
+console.log(typeof x);
+
+let distance = 8600;
+let speed = 120;
+let duraction = distance / speed;
+let minutes = duraction * 60;
+console.log("The car will arrive in China in " + minutes + " minutes");
+
+let yearBirth = "09.10.2002";
+let yearThis = "30.10.2023";
+
+console.log("I will be either " + yearBirth + " or " + yearThis);
+
+let aziza = 55;
+let nargiz = 45;
+let aysel = 50;
+let gulnur = 60;
+let collection = aziza + nargiz + aysel + gulnur;
+let division = collection / 4;
+console.log(division);
+
+let admin;
+let name="Aysel";
+admin=name
+console.log(admin);
+
+
+
