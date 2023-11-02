e435b8a2d44d20ee15c4e260e09cf8d84b2308c7
aghalarova <agalarovasbin76@gmail.com>
2023-11-02 08:48:13 -0700

    main.js

---
+++ 04.Math.library/main.js
@@ -0,0 +1,9 @@
+// console.log(Math.floor(17.23));
+// console.log(Math.ceil(17.23));
+// console.log(Math.round(17.23));
+
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(Math.round(Math.random()*30+20)); 
+}
