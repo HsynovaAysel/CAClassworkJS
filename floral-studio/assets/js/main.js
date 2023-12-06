5b514e5c7d5c824d07d5bb5a107e813221f21300
HsynovaAysel <gulum.huseynova.2002@mail.ru>
2023-12-07 03:05:53 +0400

    main.js

--- floral-studio/assets/js/main.js
+++ floral-studio/assets/js/main.js
@@ -1,4 +1,4 @@
-
+// Cards
 let pricing= document.querySelector('.flowers-pricing-image')
 async function getData () {
     let res = await axios(`http://localhost:8080/products`)
@@ -12,8 +12,59 @@ function drawCards(array) {
     array.forEach(el => {
         pricing.innerHTML+=`<div class="card-image">
         <img src="${el.image}" alt="">
-        <h3>${el.title}</h3>
+      <div class="title-icon">
+      <h3>${el.title}</h3>
+      <i class="${
+        favorites.find((item) => item.id === el.id)
+          ? "fa-solid fa-heart"
+          : "fa-regular fa-heart"
+      }" onclick=heart(this,${el.id})></i>
+        </div>
         <p>$${el.price}</p>
       </div>`
     });
 }
+
+
+let heartIcon = document.querySelector(".fa-heart");
+let supNum = document.querySelector(".count");
+
+
+
+function heart(icon, id) {
+  if (icon.className === "fa-solid fa-heart") {
+    icon.className = "fa-regular fa-heart";
+    favorites = favorites.filter((item) => item.id !== id);
+  } else {
+    icon.className = "fa-solid fa-heart";
+    let find = data.find((item) => item.id === id);
+    favorites.push(find);
+  }
+  count();
+  setToLocalStorage(favorites);
+}
+
+function setToLocalStorage(arr) {
+  localStorage.setItem("favorites", JSON.stringify(arr));
+}
+function getFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("favorites")) ?? [];
+}
+
+function count() {
+  supNum.textContent = favorites.length;
+}
+count();
+
+// navBar
+let nav=document.querySelector('nav')
+let menuIcon=document.querySelector('.fa-bars')
+
+menuIcon.addEventListener('click',function () {
+    nav.classList.toggle("show");
+if(menuIcon.classList.contains('fa-bars')){
+    menuIcon.className='fa solid fa-xmark'  
+}else{
+    menuIcon.className='fa solid fa-bars'
+}    
+})
